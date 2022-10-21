c4bee08c91c3ba7044c99010469d8afdab2d422a
Arthur Cáceres Melo <arthurcaceres@gmail.com>
2022-10-21 18:07:50 -0300

    Criando requisições GET e POST e inserindo no banco via Postman

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -14,7 +14,8 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        //
+        $marcas = Marca::all();
+        return $marcas;
     }
 
     /**
@@ -31,7 +32,8 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $marca = Marca::create($request->all());
+        return $marca;
     }
 
     /**
@@ -42,7 +44,7 @@ class MarcaController extends Controller
      */
     public function show(Marca $marca)
     {
-        //
+        return $marca;
     }
 
     /**
